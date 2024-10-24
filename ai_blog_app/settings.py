3c83b97850f743ec87569f93aa599dac7272513b
Samuel Wahome <150454830+Michinji-Samuel@users.noreply.github.com>
2024-10-24 13:14:41 +0300

    database passkey hide

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'samwahome7',
+        'PASSWORD': os.getenv('database_pass'),
         'HOST': 'localhost',
         'PORT': '5432'
     }
